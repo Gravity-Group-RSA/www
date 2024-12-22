f42d9a708835143c9e59dd73d433ff074a69cd87
burnt-exe <126121348+burnt-exe@users.noreply.github.com>
2024-12-22 21:38:35 +0200

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  }
+  base: '/www/', // Add this line - it should match your repository name
 })
