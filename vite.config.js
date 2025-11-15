e9ce6626edbef72f312b8d332a36bc633c5c46a9
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2025-11-15 05:39:49 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/www/', // Add this line - it should match your repository name
-})
+
+  // GitHub Pages requires a base path matching your repo name
+  base: "/gravity-group-rsa/",
+
+  build: {
+    outDir: "dist",
+  },
+});
